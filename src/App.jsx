77c7694deee229bcc1c46287c9570d20409dba17
Abaljerind <abaljerinda@gmail.com>
2025-07-26 14:10:08 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    (document.body.style.backgroundColor = "white"),
+    (
+      <>
+        <h1 className="text-3xl font-bold text-[#25283a] underline">DevLens</h1>
+
+        <p className="text-[#25283a]">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+          necessitatibus fugit voluptas dolore iste! Magni vero quae iure
+          voluptatum sit!
+        </p>
+
+        <div className="mx-auto my-8 h-8 w-8 rounded-lg bg-[#ededed]"></div>
+      </>
+    )
+  );
+}
+
+export default App;
