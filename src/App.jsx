f7899dad06e307499237978b0fbe320347002eb2
Abaljerind <abaljerinda@gmail.com>
2025-07-28 11:42:08 +0700

    Fix(App): Add div and import NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    (document.body.style.backgroundColor = "white"),
-    (
-      <>
-        <h1 className="text-3xl font-bold text-[#25283a] underline">DevLens</h1>
-
-        <p className="text-[#25283a]">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
-          necessitatibus fugit voluptas dolore iste! Magni vero quae iure
-          voluptatum sit!
-        </p>
-
-        <div className="mx-auto my-8 h-8 w-8 rounded-lg bg-[#ededed]"></div>
-      </>
-    )
+    <div className="p-4">
+      <NavBar />
+    </div>
   );
 }
 
