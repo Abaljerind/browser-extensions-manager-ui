34bba0b9b12e697fa9f850296a66eee6744f03fa
AbalJerind <abaljerinda@gmail.com>
2025-07-29 06:28:16 +0700

    Add Attribution component as footer

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,22 @@
+const Attribution = () => {
+  return (
+    <footer>
+      <p className="attribution text-title mt-8 text-center">
+        Challenge by{" "}
+        <a
+          className="hover:text-red-400"
+          href="https://www.frontendmentor.io?ref=challenge"
+        >
+          Frontend Mentor
+        </a>
+        . <br /> Coded with ⚛️ by{" "}
+        <a className="hover:text-red-400" href="https://github.com/Abaljerind/">
+          AbalJerind
+        </a>
+        .
+      </p>
+    </footer>
+  );
+};
+
+export default Attribution;
