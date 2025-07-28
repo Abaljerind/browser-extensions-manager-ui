4e02955243baa066d37315e0bd69554015486b30
Abaljerind <abaljerinda@gmail.com>
2025-07-28 18:16:58 +0700

    Fix(NavBar): change NavBar wrapper from <div> to <nav>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,13 @@ const NavBar = () => {
   const { darkMode, setDarkMode } = useTheme();
 
   return (
-    <div className="bg-card-filter flex items-center justify-between rounded-lg p-3 shadow-md">
+    <nav className="bg-card-filter flex items-center justify-between rounded-lg p-3 shadow-md">
       <img
         src={!darkMode ? "./images/logo.svg" : "./images/logo-dark.svg"}
         alt="logo extensions"
       />
       <ThemeToggle />
-    </div>
+    </nav>
   );
 };
 
