788c658460254a3c40c5059476ad3ccb006dfc0a
Abaljerind <abaljerinda@gmail.com>
2025-07-28 17:38:49 +0700

    Feat(App): import ExtensionsList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ExtensionsList from "./components/ExtensionsList";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="p-4">
       <NavBar />
+      <ExtensionsList />
     </div>
   );
 }
