b5f2554ffb14106685f9de6638f01a5995c0dab3
Abaljerind <abaljerinda@gmail.com>
2025-08-06 20:03:29 +0700

    Style(App): add styles for lg, xl and 2xl breakpoint
    Adjust padding inline and padding block for <div> for lg, xl and 2xl breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="p-4">
+    <div className="p-4 lg:px-14 lg:py-8 xl:px-24 xl:py-12 2xl:mx-auto 2xl:max-w-[1440px]">
       <NavBar />
       <ExtensionsList />
     </div>
