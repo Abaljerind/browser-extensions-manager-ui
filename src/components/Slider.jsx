cad4a0a164c16d5e047fc13158aa53cfc9218ea0
Abaljerind <abaljerinda@gmail.com>
2025-08-11 11:39:28 +0700

    Style(Slider): adjust style focus state for <div>
    Adjust utility peer to <input>
    Adjust outline width, color and offset with peer-focus to <div>

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,12 +3,12 @@ const Slider = ({ extension, onToggle }) => {
     <label className="relative inline-flex cursor-pointer items-center">
       <input
         type="checkbox"
-        className="sr-only"
+        className="peer sr-only"
         checked={extension}
         onChange={onToggle}
       />
       <div
-        className={`${extension ? "bg-toggle-active" : "bg-toggle-inactive"} h-5 w-9 rounded-full duration-150`}
+        className={`${extension ? "bg-toggle-active" : "bg-toggle-inactive"} peer-focus:outline-toggle-active peer-focus:outline-offset-2; h-5 w-9 rounded-full duration-150 peer-focus:outline-2`}
       >
         <div
           className={`${extension ? "translate-x-[16px]" : ""} absolute top-0.5 left-0.5 h-4 w-4 transform rounded-full bg-white duration-300`}
