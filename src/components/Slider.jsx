66f8e2e5b47c73a9dffb9cf5d47c54ee304c83ce
Abaljerind <abaljerinda@gmail.com>
2025-08-11 12:07:07 +0700

    Style(Slider & index.css): change focus state to focus-visible

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -8,7 +8,7 @@ const Slider = ({ extension, onToggle }) => {
         onChange={onToggle}
       />
       <div
-        className={`${extension ? "bg-toggle-active" : "bg-toggle-inactive"} peer-focus:outline-toggle-active peer-focus:outline-offset-2; h-5 w-9 rounded-full duration-150 peer-focus:outline-2`}
+        className={`${extension ? "bg-toggle-active" : "bg-toggle-inactive"} peer-focus-visible:outline-toggle-active peer-focus-visible:outline-offset-2; h-5 w-9 rounded-full duration-150 peer-focus-visible:outline-2`}
       >
         <div
           className={`${extension ? "translate-x-[16px]" : ""} absolute top-0.5 left-0.5 h-4 w-4 transform rounded-full bg-white duration-300`}
